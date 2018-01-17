aa2083f1bfef7c01bf6d339b21c99c21d187982b
unknown <thangddnt@gmail.com>
2018-01-17 20:08:37 +0900

    Updated

---
+++ 04-Loop/dataloop.py
@@ -0,0 +1,36 @@
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'bonn', 
+          'norway':'oslo', 'italy':'rome', 'poland':'warsaw', 'australia':'vienna' }
+          
+# Iterate over europe
+for key,value in europe.items():
+    print("the capital of "+str(key)+" is "+str(value))
+
+# Import numpy as np
+import numpy as np
+
+# For loop over np_height 
+for h in np_height:
+    print(str(h)+" inches")
+
+# For loop over np_baseball that is 2D Numpy array
+for b in np.nditer(np_baseball):
+    print(str(b))
+
+
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Iterate over rows of cars
+for rlabel, rcontent in cars.iterrows():
+    print(rlabel)
+    print(rcontent)
+
+# Adapt for loop
+for lab, row in cars.iterrows() :
+    print(lab+": "+str(row['cars_per_cap']))
+
+# Code for loop that adds COUNTRY column
+for rlabel,rcontent in cars.iterrows():
+    cars.loc[rlabel, "COUNTRY"]=str(rcontent["country"]).upper()
+# Print cars
+print(cars)
