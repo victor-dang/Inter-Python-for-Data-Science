d4de6db2a9a528175837d764ec2c4d657f64bdd2
unknown <thangddnt@gmail.com>
2018-01-17 18:54:15 +0900

    pandas data filtering

--- 02-Logic-ControlFlow/filtering pandas.py
+++ 02-Logic-ControlFlow/filtering pandas.py
@@ -12,3 +12,12 @@ sel = cars[dr]
 print(sel)
 
 #sel = cars[cars['drives_right']]
+
+# Create car_maniac: observations that have a cars_per_cap over 500
+cpc = cars["cars_per_cap"]
+many_cars = cpc > 500
+car_maniac = cars[many_cars]
+
+
+# Print car_maniac
+print(car_maniac)
